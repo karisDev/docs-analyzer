0281cd6832603a4c43f3581e636b77268eba3d39
karisDev <karjaakirill@gmail.com>
2022-12-05 00:42:56 +0300

    z index fix, card with no design

---
+++ src/components/cards/UploadCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UploadCard = () => {
+  return <div className="upload_card">UploadCarTed</div>;
+};
+
+export default UploadCard;
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import TopNavbar from "../components/ui/TopNavbar";
 import { ReactComponent as DataTransferSvg } from "../assets/waves.svg";
+import UploadCard from "../components/cards/uploadCard";
 
 const MainPage = () => {
   return (
@@ -8,7 +9,12 @@ const MainPage = () => {
       <div className="wave_bg">
         <DataTransferSvg />
       </div>
-      <TopNavbar title={"отчёт"} />
+      <div className="content">
+        <TopNavbar title={"отчёт"} />
+        <div className="upload_holder">
+          <UploadCard />
+        </div>
+      </div>
     </div>
   );
 };
