8e5106384d4c00e61266a7e1d05ceb4113be1b6d
karisDev <karjaakirill@gmail.com>
2022-12-09 20:24:42 +0300

    upload card

--- src/components/cards/UploadCard.jsx
+++ src/components/cards/UploadCard.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 const UploadCard = () => {
-  return <div className="upload_card">UploadCarTed</div>;
+  return (
+    <div className="upload_card">
+      <h2>ЗАГРУЗКА</h2>
+      <div className="content">
+        <p>
+          Для анализа документа перенесите его
+          <br />
+          <span>(.pdf .word .wordx)</span>
+        </p>
+        <div className="choose">
+          <div className="option">
+            <div className="sep" />
+            <p>ИЛИ</p>
+            <div className="sep" />
+          </div>
+          <button className="main_btn">Выбрать документ</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default UploadCard;
